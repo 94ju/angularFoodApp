d28c4b791d0fc6ea1eedc2f72220dcbd1ed044d4
94ju <janithherath94@gmail.com>
2019-05-05 10:29:18 +0530

    recipe list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -2,12 +2,14 @@ import {Component} from '@angular/core'
 import { Recipie } from '../recipe.model';
 
 @Component({
-    selector:'app-recipieList',
+    selector:'app-recipie-list',
     templateUrl:'./recipe-list.component.html'
 })
 export class RecipieListComponent{
     recipes : Recipie[] =[
         new Recipie('Pizza','Test Pizza','https://img.taste.com.au/n0QkSEPK/taste/2016/11/smoked-salmon-and-cucumber-sandwiches-79157-1.jpeg')
+    ,    new Recipie('Pizza','Test Pizza','https://img.taste.com.au/n0QkSEPK/taste/2016/11/smoked-salmon-and-cucumber-sandwiches-79157-1.jpeg')
+    
     ];   
     constructor(){}
     ngOnInit(){
