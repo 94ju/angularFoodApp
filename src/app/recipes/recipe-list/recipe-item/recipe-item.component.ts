02fb6e5dc785f66db450359a61ea35153cfe446b
94ju <janithherath94@gmail.com>
2019-05-28 23:41:57 +0530

    reciepe list component optimized

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
+import { Component,  Input } from '@angular/core';
+import { Recipie } from '../../recipe.model';
 
 @Component({
     selector:'app-recipie-item',
     templateUrl:'./recipe-item.component.html'
 })
-export class RecipeItemComponent{}
+export class RecipeItemComponent{
+    @Input() recipie: Recipie;
+    ngOnInit(){
+
+    }
+}
