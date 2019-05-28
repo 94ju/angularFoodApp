2f8a0921435b2b9aa16be439ef000cbd6ed49dbe
94ju <janithherath94@gmail.com>
2019-05-28 21:24:01 +0530

    navigating phase1

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,13 @@
-import {Component} from '@angular/core'
+import {Component, Output} from '@angular/core'
+import { EventEmitter } from '@angular/core';
 
 @Component({
     selector:'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent{
-
+    @Output() featureselected =new EventEmitter<string>();
+    onSelect(feature:string){
+       this.featureselected.emit(feature);
+    }
 }
